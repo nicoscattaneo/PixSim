4dcc211dd2b5dba8cb838a86b8048308a45d711a
Nicolas Cattaneo <nicolas.cattaneo@nibio.no>
2023-11-10 01:13:26 +0100

    imporove the raster2pixeltable documentation

--- R/raster2pixeltable.R
+++ R/raster2pixeltable.R
@@ -1,5 +1,6 @@
-#' Norwegian Forest Resource Rasters
-#'
+#' @name raster2pixeltable
+#' @title Norwegian Forest Resource Rasters
+#' @description
 #' Raster files describing various forest resource variables in a 10x20 km region
 #' in central Norway, sourced from the Norwegian Forest Resource Map SR16.
 #' These files are intended to be used to construct the `PixelTable` for the PixSim package.
@@ -18,30 +19,32 @@
 #' }
 #'
 #' Data source: \url{https://kilden.nibio.no/}
-#'
+#' @export
 #' @examples
-#' \dontrun{
 #' Dir <- system.file("extdata", package = "PixSim")
 #' Files <- list.files(Dir, full.names = TRUE, pattern = "\\.tif$")
 #' Names <- sapply(strsplit(basename(Files), "\\."), `[`, 1)
-#'
+#' # The following is an illustrative example and is not intended to be run as is.
+#' \dontrun{
 #' # Example code to read and plot the Site Index raster
 #' # Requires the 'terra' package, which is suggested but not required
-#' # library(terra)
-#' # SI_raster <- rast(file.path(Dir, "SI_m.tif"))
-#' # plot(SI_raster)
+#' library(terra)
+#' SI_raster <- rast(file.path(Dir, "SI_m.tif"))
+#' plot(SI_raster)
 #'
 #' # Illustrative example to construct PixelTable from rasters
-#' # This example is for illustrative purposes and is not intended to be run
-#' # as the PixSim package does not depend on 'terra'
-#'
-#' # Assuming terra package is installed and loaded with library(terra)
-#' # Files <- lapply(Files, function(XX){
-#' #   terra::as.data.frame(terra::rast(XX), xy = TRUE)[, 1:3]
-#' # })
-#' # Files <- Reduce(function(...) merge(..., all = FALSE), Files)
-#' # names(Files) <- c("x_UTM32", "y_UTM32", Names)
-#' # Files$H_m <- Files$H_dm * 0.1
-#' # Files$H_dm <- NULL
-#' # head(Files)
+#' Files <- lapply(Files, function(XX){
+#'   as.data.frame(rast(XX), xy = TRUE)[, 1:3]
+#' })
+#' Files <- Reduce(function(...) merge(..., all = FALSE), Files)
+#' names(Files) <- c("x_UTM32", "y_UTM32", Names)
+#' Files$H_m <- Files$H_dm * 0.1
+#' Files$H_dm <- NULL
+#' head(Files)
 #' }
+raster2pixeltable <- function() {
+  Dir <- system.file("extdata", package = "PixSim")
+  Files <- list.files(Dir, full.names = TRUE, pattern = "\\.tif$")
+  Names <- sapply(strsplit(basename(Files), "\\."), `[`, 1)
+  return(list(Names = Names, Paths = Files))
+}
